9787e12f2ecfa8d3ae10f8316764efd57e6b5da5
denis ananev <da@forgott.net>
2026-02-03 09:20:12 +0100

    fix: apply_chunking_to_forward moved to pytorch_utils in transformers 5.x

--- src/vggsound_pipeline/qwenvl/model/modeling_bert.py
+++ src/vggsound_pipeline/qwenvl/model/modeling_bert.py
@@ -39,10 +39,15 @@ from transformers.modeling_outputs import (
 )
 from transformers.modeling_utils import (
     PreTrainedModel,
-    apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
     prune_linear_layer,
 )
+
+# apply_chunking_to_forward moved to pytorch_utils in transformers 5.x
+try:
+    from transformers.pytorch_utils import apply_chunking_to_forward
+except ImportError:
+    from transformers.modeling_utils import apply_chunking_to_forward
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
