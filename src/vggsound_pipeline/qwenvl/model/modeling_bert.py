ca3809f1bbc136c9e59d2a975ff0166dc63564e3
denis ananev <da@forgott.net>
2026-02-03 11:13:41 +0100

    fix: handle missing initializer_range in sub-configs

--- src/vggsound_pipeline/qwenvl/model/modeling_bert.py
+++ src/vggsound_pipeline/qwenvl/model/modeling_bert.py
@@ -692,7 +692,7 @@ class BertPreTrainedModel(PreTrainedModel):
         if isinstance(module, (nn.Linear, nn.Embedding)):
             # Slightly different from the TF version which uses truncated_normal for initialization
             # cf https://github.com/pytorch/pytorch/pull/5617
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            module.weight.data.normal_(mean=0.0, std=getattr(self.config, "initializer_range", 0.02))
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
